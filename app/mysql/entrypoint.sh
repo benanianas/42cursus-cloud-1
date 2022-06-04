b986da604c822a5cfa5f852db2749ff1985d34bf
Anas Benani <benanianas21@gmail.com>
2022-06-04 16:57:50 +0100

    compose

--- app/mysql/entrypoint.sh
+++ app/mysql/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+
+if ! [ -d /var/lib/mysql/${DB_NAME} ] ; then 
 usermod -d /var/lib/mysql/ mysql
 service mysql start
 mysql -u root -e "CREATE DATABASE $DB_NAME" > /dev/null 2>&1
-mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD'" > /dev/null 2>&1
-mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_USER'@'%'" > /dev/null 2>&1
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '$MYSQL_ROOT_PASSWORD'" > /dev/null 2>&1
+mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED WITH mysql_native_password by '$MYSQL_PASSWORD'" > /dev/null 2>&1
+mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION" > /dev/null 2>&1
 mysql -u root -e "FLUSH PRIVILEGES" > /dev/null 2>&1
-
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH BY '$MYSQL_ROOT_PASSWORD'" > /dev/null 2>&1
 service mysql stop
+fi
+
 exec "$@"
--- app/wordpress/entrypoint.sh
+++ app/wordpress/entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-sed -i 's/post_max_size = 8M/post_max_size = 64M/g' /etc/php/7.3/fpm/php.ini
-sed -i 's/memory_limit = 128M/memory_limit = 256M/g' /etc/php/7.3/fpm/php.ini
-sed -i 's/max_execution_time = 30/max_execution_time = 300/g' /etc/php/7.3/fpm/php.ini
-sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 32M/g' /etc/php/7.3/fpm/php.ini
-sed -i 's/;date.timezone =/date.timezone = \"Africa\/Casablanca\"/g' /etc/php/7.3/fpm/php.ini
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 5000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
+if ! [ -d /var/www/wordpress ] ; then
+mkdir -p /var/www/wordpress
 wp core download --path=/var/www/wordpress --version=6.0 --allow-root --quiet
 wp core config --path=/var/www/wordpress --dbname=$DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mysql --skip-check --allow-root --quiet
 wp core install --path=/var/www/wordpress --url=localhost --title=abenani --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=abenani@student.1337.ma --allow-root --quiet
+fi
 
 exec "$@"
