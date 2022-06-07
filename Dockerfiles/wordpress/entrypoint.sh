d401cdb484ff04a846a685af9d37a9fecd6dd7a8
root <benaniaanas21@gmail.com>
2022-06-07 09:26:05 +0000

    containers running

--- Dockerfiles/wordpress/entrypoint.sh
+++ Dockerfiles/wordpress/entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-if ! [ -d /var/www/wordpress ] ; then
-mkdir -p /var/www/wordpress
+if ! [ -d /var/www/wordpress/index.php ] ; then
 wp core download --path=/var/www/wordpress --version=6.0 --allow-root --quiet
 wp core config --path=/var/www/wordpress --dbname=$DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mysql --skip-check --allow-root --quiet
-wp core install --path=/var/www/wordpress --url=localhost --title=abenani --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=abenani@student.1337.ma --allow-root --quiet
+wp core install --path=/var/www/wordpress --url=$HOST --title=abenani --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=abenani@student.1337.ma --allow-root --quiet
 fi
 
 exec "$@"
