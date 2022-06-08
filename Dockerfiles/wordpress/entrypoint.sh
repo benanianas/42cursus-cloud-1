b5fb7b312afbdcb3a2d77148433d48da7ba1caeb
root <benaniaanas21@gmail.com>
2022-06-08 17:53:18 +0000

    ip

--- Dockerfiles/wordpress/entrypoint.sh
+++ Dockerfiles/wordpress/entrypoint.sh
@@ -3,7 +3,7 @@
 if ! [ -d /var/www/wordpress/index.php ] ; then
 wp core download --path=/var/www/wordpress --version=6.0 --allow-root --quiet
 wp core config --path=/var/www/wordpress --dbname=$DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mysql --skip-check --allow-root --quiet
-wp core install --path=/var/www/wordpress --url=$HOST_IP --title=abenani --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=abenani@student.1337.ma --allow-root --quiet
+wp core install --path=/var/www/wordpress --url=$HOST_DOMAIN --title=$HOST_IP --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=abenani@student.1337.ma --allow-root --quiet
 fi
 
 exec "$@"
